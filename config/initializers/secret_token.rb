8eb00352117f871b00fb4cf22f9102b80515dc67
Jon Hinson <hinson.jonathan@gmail.com>
2011-05-20 22:48:33 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WwfCheat::Application.config.secret_token = '3ce873674900579d9dfe9c02ddb0844233883e965981be70f022e0ecd0261bbd8d4baed103d7a5d2dd1d288deb3cc0fa164b09a1aefc42f3d35bd0e835de662f'
